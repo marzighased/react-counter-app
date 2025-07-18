0f8fa57c749c08fc18627c9faa02f1db9c4c64bd
Marzi Ghasedian <marzighased@gmail.com>
2025-07-18 21:37:44 +0200

    reset button added

--- src/App.js
+++ src/App.js
@@ -12,12 +12,19 @@ function App() {
     setCount(count - 1);
   };
 
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
     <div className="container">
       <h2>Counter App</h2>
       <button className="counter-button" onClick={increase}>+</button>
       <h1 className="counter-display">{count}</h1>
       <button className="counter-button" onClick={decrease}>-</button>
+
+      <br /><br />
+      <button className="reset-button" onClick={reset}>Reset</button>
       
     </div>
   );
